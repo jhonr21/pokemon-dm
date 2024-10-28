67b0a473fff713e298ec8cd76822ed270dc7cc66
darcinmilena.becerra.contractor.cidentity <DARCINMILENA.BECERRA.CONTRACTOR.CIDENTITY@BBVA.COM>
2024-10-28 17:43:12 -0500

    Describe los cambios realizados

--- src/PokemonDm.js
+++ src/PokemonDm.js
@@ -1,46 +1,102 @@
-import { LitElement, html } from 'lit-element';
-import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
-import styles from './pokemon-dm.css.js';
+import { LitElement } from 'lit-element';
 
-/**
- * ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
- *
- * This component ...
- *
- * Example:
- *
- * ```html
- *   <pokemon-dm></pokemon-dm>
- * ```
- */
 export class PokemonDm extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Description for property
-       */
-      name: {
-        type: String,
-      },
-    };
-  }
-
   constructor() {
     super();
-    this.name = 'Cells';
+    this.pokemons = [];
   }
 
-  static get styles() {
-    return [
-      styles,
-      getComponentSharedStyles('pokemon-dm-shared-styles'),
-    ];
+  async fetchPokemons() {
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=90');
+      const data = await response.json();
+
+      this.pokemons = await Promise.all(
+        data.results.map(async (pokemon) => {
+          const detailResponse = await fetch(pokemon.url);
+          const detailData = await detailResponse.json();
+          return {
+            name: detailData.name,
+            id: detailData.id,
+            image: detailData.sprites.other.dream_world.front_default,
+            type: detailData.types.map((type) => type.type.name).join(', '),
+            evolutionChain: await this.fetchEvolutionChain(detailData.species.url),
+          };
+        })
+      );
+
+      this.dispatchEvent(new CustomEvent('pokemon-data', { 
+        detail: { pokemons: this.pokemons }, 
+        bubbles: true, 
+        composed: true 
+      }));
+    } catch (error) {
+      console.error('Error fetching Pokémon data:', error);
+    }
   }
 
-  render() {
-    return html`
-      <p>Welcome to ${this.name}</p>
-      <slot></slot>
-    `;
+  async fetchEvolutionChain(speciesUrl) {
+    const response = await fetch(speciesUrl);
+    const speciesData = await response.json();
+    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionResponse.json();
+
+    return this.processEvolutionChain(evolutionData);
+  }
+
+  processEvolutionChain(evolutionData) {
+    const evolutions = [];
+    let current = evolutionData.chain;
+
+    while (current) {
+      evolutions.push({
+        name: current.species.name,
+        // URL de la imagen basada en el nombre del Pokémon
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${current.species.name}.svg`,
+        type: current.species.name,
+      });
+      current = current.evolves_to[0];
+    }
+
+    return evolutions;
+  }
+
+  firstUpdated() {
+    this.fetchPokemons();
+  }
+
+
+async processEvolutionChain(evolutionData) {
+  const evolutions = [];
+  let current = evolutionData.chain;
+
+  while (current) {
+    const name = current.species.name;
+    const pokemonDataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const pokemonData = await pokemonDataResponse.json();
+
+    evolutions.push({
+      name: name,
+      image: pokemonData.sprites.other.dream_world.front_default,
+      type: pokemonData.types.map((type) => type.type.name).join(', ')
+    });
+
+    current = current.evolves_to[0];
+  }
+
+  this.sendToDm(evolutions); // Enviar al componente dm
+  return evolutions;
+}
+
+sendToDm(data) {
+  const dmElement = this.shadowRoot.querySelector('pokemon-dm');
+  if (dmElement) {
+    dmElement.evolutions = data; // Asignación directa o con propiedad reactiva
   }
 }
+
+
+
+}
+
+customElements.define('pokemon-dm', PokemonDm);
